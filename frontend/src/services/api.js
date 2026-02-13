988f28336f066995345d36f9c5a7bf00f3ff76eb
adarshdas1217-spec <adarshdas1217@gmail.com>
2026-02-13 06:57:25 +0530

    Prepared frontend for production deployment

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
-const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+const instance = axios.create({
+  baseURL: `${import.meta.env.VITE_API_URL}/api`,
 });
 
-api.interceptors.request.use((config) => {
+instance.interceptors.request.use(config => {
   const token = localStorage.getItem("token");
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
@@ -12,4 +12,5 @@ api.interceptors.request.use((config) => {
   return config;
 });
 
-export default api;
+export default instance;
+
